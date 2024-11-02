41773cefc804317bef1e30613e45c8ba5d507af0
Barandelaa <danielbaracacha@gmail.com>
2024-11-02 11:16:30 +0100

    Finiquitado (?)

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ env.load("templatesrules2.clp")
 
 # Cargar los hechos
 # env.load("factos2.clp")
+def cargar_hechos():
+    env.assert_string('(usuario (nombre "Josefina") (ocupacion "limpieza") (acceso 2))')
+    env.assert_string('(usuario (nombre "Javier") (ocupacion "tecnico") (acceso 2))')
+    env.assert_string('(usuario (nombre "David") (ocupacion "scrum master") (acceso 3))')
+    env.assert_string('(usuario (nombre "Daniel") (ocupacion "oficinista") (acceso 1))')
+    env.assert_string('(usuario (nombre "Tomas") (ocupacion "terrorista") (acceso 0))')
 
-env.assert_string('(usuario (nombre "Josefina") (ocupacion "limpieza") (acceso 2))')
-env.assert_string('(usuario (nombre "Javier") (ocupacion "tecnico") (acceso 2))')
-env.assert_string('(usuario (nombre "David") (ocupacion "scrum master") (acceso 3))')
-env.assert_string('(usuario (nombre "Daniel") (ocupacion "oficinista") (acceso 1))')
-env.assert_string('(usuario (nombre "Tomas") (ocupacion "terrorista") (acceso 0))')
-
-env.assert_string('(zona (nombre "area-descanso") (acceso 1) (ocupacion-max 10) (ocupacion-actual 0) (contenido "no-sensible"))')
-env.assert_string('(zona (nombre "entrada") (acceso 1) (ocupacion-max 20) (ocupacion-actual 0) (contenido "no-sensible"))')
-env.assert_string('(zona (nombre "limpieza") (acceso 2) (ocupacion-max 5) (ocupacion-actual 0) (contenido "sensible"))')
-env.assert_string('(zona (nombre "oficinas") (acceso 1) (ocupacion-max 15) (ocupacion-actual 0) (contenido "sensible"))')
-env.assert_string('(zona (nombre "servidores") (acceso 2) (ocupacion-max 3) (ocupacion-actual 2) (contenido "sensible"))')
-env.assert_string('(zona (nombre "comunicaciones") (acceso 2) (ocupacion-max 4) (ocupacion-actual 0) (contenido "sensible"))')
-env.assert_string('(zona (nombre "area-control") (acceso 3) (ocupacion-max 2) (ocupacion-actual 0) (contenido "sensible"))')
+    env.assert_string('(zona (nombre "area-descanso") (acceso 1) (ocupacion-max 10) (ocupacion-actual 0) (contenido "no-sensible"))')
+    env.assert_string('(zona (nombre "entrada") (acceso 1) (ocupacion-max 20) (ocupacion-actual 0) (contenido "no-sensible"))')
+    env.assert_string('(zona (nombre "limpieza") (acceso 2) (ocupacion-max 5) (ocupacion-actual 0) (contenido "sensible"))')
+    env.assert_string('(zona (nombre "oficinas") (acceso 1) (ocupacion-max 15) (ocupacion-actual 0) (contenido "sensible"))')
+    env.assert_string('(zona (nombre "servidores") (acceso 2) (ocupacion-max 3) (ocupacion-actual 3) (contenido "sensible"))')
+    env.assert_string('(zona (nombre "comunicaciones") (acceso 2) (ocupacion-max 4) (ocupacion-actual 0) (contenido "sensible"))')
+    env.assert_string('(zona (nombre "area-control") (acceso 3) (ocupacion-max 2) (ocupacion-actual 0) (contenido "sensible"))')
 
 
 # Función para generar lecturas de temperatura
@@ -32,6 +32,7 @@ def generar_lectura(sensor):
     return {"nombre": sensor["nombre"], "zona": sensor["zona"], "temperatura": value}
 
 def demostracion_temperatura():
+    cargar_hechos()
     print("Demostración de temperatura: ")
     env.assert_string('(sensor-temperatura (nombre "sensor-temp-1") (zona "servidores") (temperatura 27))')
     env.assert_string('(sensor-temperatura (nombre "sensor-temp-2") (zona "servidores") (temperatura 18))')
@@ -40,6 +41,7 @@ def demostracion_temperatura():
     sleep(5)
 
 def demostracion_humedad():
+    cargar_hechos()
     print("Demostración de humedad: ")
     env.assert_string('(sensor-humedad (nombre "sensor-hum-1") (zona "servidores") (humedad 20))')
     env.assert_string('(sensor-humedad (nombre "sensor-hum-2") (zona "servidores") (humedad 90))')
@@ -48,6 +50,7 @@ def demostracion_humedad():
     sleep(5)
 
 def demostracion_incendio():
+    cargar_hechos()
     print("Demostración de incendio: ")
     env.assert_string('(sensor-humo (nombre "sensor-humo-1") (zona "servidores") (humo "Si"))')
     env.run()
@@ -55,6 +58,7 @@ def demostracion_incendio():
     sleep(5)
 
 def demostracion_accesos():
+    cargar_hechos()
     print("Demostración de accesos: ")
     env.assert_string('(sensor-acceso (nombre "sensor-acc-1") (zona "servidores") (acceso "Tomas"))')
     env.assert_string('(sensor-acceso (nombre "sensor-acc-2") (zona "servidores") (acceso "David"))')
@@ -64,15 +68,14 @@ def demostracion_accesos():
     sleep(5)
 
 def demostracion_bomba():
+    cargar_hechos()
     print("Demostración de bomba: ")
     env.assert_string('(sensor-metales (nombre "sensor-metales-1") (zona "entrada"))')
     env.run()
     print("\n")
-    
 
 
-#for fact in env.facts():
-#    print(fact)
+
 
 
 #Lanzar demostraciones
@@ -81,3 +84,6 @@ demostracion_humedad()
 demostracion_incendio()
 demostracion_accesos()
 demostracion_bomba()
+
+#for fact in env.facts():
+#    print(fact)
