23b076f39425c7019b97b4f41d27ed8fc2656a35
Barandelaa <danielbaracacha@gmail.com>
2024-11-03 17:19:52 +0100

    Solucionado problema para cargar hechos

--- main.py
+++ main.py
@@ -11,19 +11,8 @@ env.load("templatesrules2.clp")
 # Cargar los hechos
 # env.load("factos2.clp")
 def cargar_hechos():
-    env.assert_string('(usuario (nombre "Josefina") (ocupacion "limpieza") (acceso 2))')
-    env.assert_string('(usuario (nombre "Javier") (ocupacion "tecnico") (acceso 2))')
-    env.assert_string('(usuario (nombre "David") (ocupacion "scrum master") (acceso 3))')
-    env.assert_string('(usuario (nombre "Daniel") (ocupacion "oficinista") (acceso 1))')
-    env.assert_string('(usuario (nombre "Tomas") (ocupacion "terrorista") (acceso 0))')
-
-    env.assert_string('(zona (nombre "area-descanso") (acceso 1) (ocupacion-max 10) (ocupacion-actual 0) (contenido "no-sensible"))')
-    env.assert_string('(zona (nombre "entrada") (acceso 1) (ocupacion-max 20) (ocupacion-actual 0) (contenido "no-sensible"))')
-    env.assert_string('(zona (nombre "limpieza") (acceso 2) (ocupacion-max 5) (ocupacion-actual 0) (contenido "sensible"))')
-    env.assert_string('(zona (nombre "oficinas") (acceso 1) (ocupacion-max 15) (ocupacion-actual 0) (contenido "sensible"))')
-    env.assert_string('(zona (nombre "servidores") (acceso 2) (ocupacion-max 3) (ocupacion-actual 3) (contenido "sensible"))')
-    env.assert_string('(zona (nombre "comunicaciones") (acceso 2) (ocupacion-max 4) (ocupacion-actual 0) (contenido "sensible"))')
-    env.assert_string('(zona (nombre "area-control") (acceso 3) (ocupacion-max 2) (ocupacion-actual 0) (contenido "sensible"))')
+    env.load("hechos.clp")
+    env.reset()
 
 
 # Función para generar lecturas de temperatura
